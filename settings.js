f6f8b78b2388132bae092b698db239d705818697
Kaiilo <147712431+Kaiilo@users.noreply.github.com>
2023-10-12 20:56:39 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,6 +7,20 @@ global.author = '?'// nama author
 global.own = '?' //nama lu
 global.bot = '?'//nama bot
 
+global.mess = {
+    success: 'success!',
+    admin: 'This feature could be used by admins only!',
+    botAdmin: 'Bot Must Be Admin First!',
+    premime: 'Premium Special Features If You Want to Register Type Rent',
+    owner: 'This feature could be used by owner only',
+    group: 'Features Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This feature could be used by bot only',
+    wait: 'proses...',
+    linkm: 'Where is the link?',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+}
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
@@ -15,4 +29,5 @@ console.log(chalk.yellowBright(`Update File Terbaru ${__filename}`))
 delete require.cache[file]
 require(file)
 })
+require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
 
